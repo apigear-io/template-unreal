6ecd346578619c230d35b37a387ab6adbd8662db
Wolfgang Bremer <wbremer@apigear.io>
2025-04-09 16:18:15 +0200

    feat(msgbus): make heartbeat adjustable in setting

--- goldenmaster/Plugins/TbSame1/Source/TbSame1/Private/Generated/MsgBus/TbSame1SameEnum2InterfaceMsgBusAdapter.cpp
+++ goldenmaster/Plugins/TbSame1/Source/TbSame1/Private/Generated/MsgBus/TbSame1SameEnum2InterfaceMsgBusAdapter.cpp
@@ -24,12 +24,12 @@ limitations under the License.
 #include "Generated/MsgBus/TbSame1SameEnum2InterfaceMsgBusMessages.h"
 #include "Async/Future.h"
 #include "Async/Async.h"
-#include "Async/Async.h"
-#include "Engine/Engine.h"
+#include "Engine/World.h"
 #include "TimerManager.h"
 #include "MessageEndpoint.h"
 #include "MessageEndpointBuilder.h"
 #include "Misc/DateTime.h"
+#include "TbSame1Settings.h"
 
 DEFINE_LOG_CATEGORY(LogTbSame1SameEnum2InterfaceMsgBusAdapter);
 UTbSame1SameEnum2InterfaceMsgBusAdapter::UTbSame1SameEnum2InterfaceMsgBusAdapter()
@@ -53,6 +53,10 @@ void UTbSame1SameEnum2InterfaceMsgBusAdapter::_StartListening()
 
 	if (!_HeartbeatTimerHandle.IsValid() && GetWorld())
 	{
+		UTbSame1Settings* settings = GetMutableDefault<UTbSame1Settings>();
+		check(settings);
+		_HeartbeatIntervalMS = settings->MsgBusHeartbeatIntervalMS;
+
 		GetWorld()->GetTimerManager().SetTimer(_HeartbeatTimerHandle, this, &UTbSame1SameEnum2InterfaceMsgBusAdapter::_CheckClientTimeouts, _HeartbeatIntervalMS / 1000.0f, true);
 	}
 
--- templates/module/Source/module/Public/pluginnamesettings.h.tpl
+++ templates/module/Source/module/Public/pluginnamesettings.h.tpl
@@ -55,4 +55,14 @@ public:
 	UPROPERTY(EditAnywhere, config, Category = OLinkConnectionSetup)
 	FString OLinkConnectionIdentifier;
 {{- end }}
+
+{{- if $.Features.msgbus }}
+
+	/** Choose the heartbeat interval in milliseconds for the msgbus connection on the service side.
+	 * On the service side this adjusts the heartbeat between client and service. The clients will receive the value from service.
+	 * On the client side this adjusts how often discovery messages are sent until a service is connected.
+	 */
+	UPROPERTY(EditAnywhere, config, Category = MsgBusConnectionSetup, meta = (DisplayName = "Heartbeat interval (in milliseconds)"))
+	uint32 MsgBusHeartbeatIntervalMS = 200;
+{{- end }}
 };
