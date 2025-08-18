8e20aaf76a3254139708efe647053767312f3ff2
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2025-08-18 15:31:57 +0200

    feat(jni): add starting android service
    Introduce a service starter class that allows starting and stopping the
    android service provided by the java template.
    An android service needs to be explicitly started and in a thread of the
    unreal application, jni backend with unreal implemnetation must be provided to it.
    The java part of serivce starter of the jni module, handles most of
    that, the added cpp class runs that code.
    The object returned by it is the java jni service backend that was
    instantionated for the service. It should be used to perform operations
    on it.

---
+++ templates/module/Source/modulejni/Private/Generated/jni/javaservicestarter.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Engine/Engine.h"
+#include <string>
+
+namespace ApiGear
+{
+class JavaServiceStarter
+{
+public:
+#if PLATFORM_ANDROID
+#if USE_ANDROID_JNI
+	static jobject startAndroidServer(std::string fullJavaClassName, std::string fullJavaUnrealServiceTypeString);
+#endif
+#endif
+	static void stopAdnroidServer(std::string fullJavaClassName);
+};
+
+}
