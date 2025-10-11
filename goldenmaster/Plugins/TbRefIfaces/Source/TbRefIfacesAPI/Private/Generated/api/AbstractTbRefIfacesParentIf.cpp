717f8f357f48183f33a776577dde8b645871351e
Wolfgang Bremer <wbremer@apigear.io>
2025-10-11 21:11:06 +0200

    refactor(api): move latentaction to private header
    The latent action can easily be re-used within the module with it being
    available in a private header.

--- goldenmaster/Plugins/TbRefIfaces/Source/TbRefIfacesAPI/Private/Generated/api/AbstractTbRefIfacesParentIf.cpp
+++ goldenmaster/Plugins/TbRefIfaces/Source/TbRefIfacesAPI/Private/Generated/api/AbstractTbRefIfacesParentIf.cpp
@@ -15,64 +15,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "TbRefIfaces/Generated/api/AbstractTbRefIfacesParentIf.h"
+#include "TbRefIfacesParentIfLatentAction.h"
 #include "Async/Async.h"
 #include "Engine/Engine.h"
-#include "Engine/LatentActionManager.h"
-#include "LatentActions.h"
-
-template <typename TAsyncResult>
-class FTbRefIfacesParentIfLatentAction : public FPendingLatentAction
-{
-private:
-	FName ExecutionFunction;
-	int32 OutputLink;
-	FWeakObjectPtr CallbackTarget;
-	TAtomic<bool> bCancelled{false};
-	TFuture<TAsyncResult> Future;
-	TAsyncResult* OutPtr;
-
-public:
-	FTbRefIfacesParentIfLatentAction(const FLatentActionInfo& LatentInfo,
-		TFuture<TAsyncResult>&& InFuture,
-		TAsyncResult& ResultReference)
-		: ExecutionFunction(LatentInfo.ExecutionFunction)
-		, OutputLink(LatentInfo.Linkage)
-		, CallbackTarget(LatentInfo.CallbackTarget)
-		, Future(MoveTemp(InFuture))
-		, OutPtr(&ResultReference)
-	{
-	}
-
-	void Cancel()
-	{
-		bCancelled.Store(true);
-	}
-
-	void UpdateOperation(FLatentResponse& Response) override
-	{
-		if (bCancelled.Load())
-		{
-			Response.DoneIf(true);
-			return;
-		}
-
-		if (Future.IsReady())
-		{
-			*OutPtr = Future.Get();
-			Response.FinishAndTriggerIf(true, ExecutionFunction, OutputLink, CallbackTarget);
-		}
-	}
-
-	void NotifyObjectDestroyed() override
-	{
-		Cancel();
-	}
-
-	void NotifyActionAborted() override
-	{
-		Cancel();
-	}
-};
 
 UAbstractTbRefIfacesParentIf::UAbstractTbRefIfacesParentIf()
 {
--- goldenmaster/Plugins/TbSame1/Source/TbSame1API/Private/Generated/api/AbstractTbSame1SameStruct2Interface.cpp
+++ goldenmaster/Plugins/TbSame1/Source/TbSame1API/Private/Generated/api/AbstractTbSame1SameStruct2Interface.cpp
@@ -15,64 +15,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "TbSame1/Generated/api/AbstractTbSame1SameStruct2Interface.h"
+#include "TbSame1SameStruct2InterfaceLatentAction.h"
 #include "Async/Async.h"
 #include "Engine/Engine.h"
-#include "Engine/LatentActionManager.h"
-#include "LatentActions.h"
-
-template <typename TAsyncResult>
-class FTbSame1SameStruct2InterfaceLatentAction : public FPendingLatentAction
-{
-private:
-	FName ExecutionFunction;
-	int32 OutputLink;
-	FWeakObjectPtr CallbackTarget;
-	TAtomic<bool> bCancelled{false};
-	TFuture<TAsyncResult> Future;
-	TAsyncResult* OutPtr;
-
-public:
-	FTbSame1SameStruct2InterfaceLatentAction(const FLatentActionInfo& LatentInfo,
-		TFuture<TAsyncResult>&& InFuture,
-		TAsyncResult& ResultReference)
-		: ExecutionFunction(LatentInfo.ExecutionFunction)
-		, OutputLink(LatentInfo.Linkage)
-		, CallbackTarget(LatentInfo.CallbackTarget)
-		, Future(MoveTemp(InFuture))
-		, OutPtr(&ResultReference)
-	{
-	}
-
-	void Cancel()
-	{
-		bCancelled.Store(true);
-	}
-
-	void UpdateOperation(FLatentResponse& Response) override
-	{
-		if (bCancelled.Load())
-		{
-			Response.DoneIf(true);
-			return;
-		}
-
-		if (Future.IsReady())
-		{
-			*OutPtr = Future.Get();
-			Response.FinishAndTriggerIf(true, ExecutionFunction, OutputLink, CallbackTarget);
-		}
-	}
-
-	void NotifyObjectDestroyed() override
-	{
-		Cancel();
-	}
-
-	void NotifyActionAborted() override
-	{
-		Cancel();
-	}
-};
 
 UAbstractTbSame1SameStruct2Interface::UAbstractTbSame1SameStruct2Interface()
 {
