eefbd94d8beaf13e780ad9033f7c341a8193a3cc
Aleksander Wawrzyniak <aleksander.wawrzyniak@siili.com>
2026-02-12 15:29:17 +0100

    fix(jni): remove stale test handle
    This test dll handle is an leftover from old designs. Remove it.

--- goldenmaster/Plugins/TbSame1/Source/TbSame1MsgBus/Public/TbSame1/TbSame1MsgBus.h
+++ goldenmaster/Plugins/TbSame1/Source/TbSame1MsgBus/Public/TbSame1/TbSame1MsgBus.h
@@ -24,8 +24,4 @@ public:
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
-
-private:
-	/** Handle to the test dll we will load */
-	void* FTbSame1MsgBusModule;
 };
